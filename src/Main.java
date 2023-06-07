b49c5e07d76b46dbf0d1a47ce69e9e4403af284b
Nuriza <kanibekovaaizi@gmail.com>
2023-06-07 10:50:32 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+  /*  ArrayList түз:
+//    ArrayList_ке элементтерди кош, элементтери шаарлардын аттары болсун:
+//    Биринчи индекстеги элементти консолго чыгар:
+//    Белгилүү бир индекстеги элементти очур:
+//    ArrayList бош экенин текшериңиз:
+//    ArrayList_тин размерин текшер:
+//    Цикл колдонуу менен ArrayList маанилерерин консолго чыгар:
+//    ArrayList_те белгилүү бир элемент бар экендигин текшериңиз: (мисалы : Bishkek)
+//    ArrayList_тин бардык элементтерин очуруп салыңыз:
+//    ArrayList_ти массивге айландыр:*/
+        ArrayList<String> city = new ArrayList<>();
+        city.add("Osh");
+        city.add("Naryn");
+        city.add("Talas");
+        city.add("Jalal-Abad");
+        city.add("Ozgon");
+        city.add("Kara-Balta");
+
+
+        String[] city1 = {"Osh", "Narun", "Talas", "Jalal-Abad", "Ozgon", "Kara-Balta"};
+        System.out.println(city1.length);
+
+    }
+
+       /* System.out.println(city.get(1));
+        System.out.println(city.remove(5));
+        System.out.println(city.isEmpty());
+        System.out.println(city.size());
+
+        System.out.println(city);
+        city.clear();
+        System.out.println(city);*/
+
+    // System.out.println(city.size());
+
+    //  for(int i=0;i< city.size();i++){
+    // System.out.println(city.get(i));
+
+
+      /*  for(String s:city){
+            System.out.println(s);*/
+
+
+}
+
+
+
+
